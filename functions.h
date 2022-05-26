23f2ba87a839bae81a3e662f25f37a72da8fda06
Hancock132 <105845679+Hancock132@users.noreply.github.com>
2022-05-26 09:42:43 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+void Izbornik(void);
+int PacMan(void);
+void Ljestvica(void);
+void Upute(void);
+void Izlaz(void);
+void UpisBodova(int);
+void PokusajPonovno(int);
+void Zamjena(PLAYER*, PLAYER*);
+void SelectionSortIme(PLAYER*, const int);
+void SelectionSortBodovi(PLAYER*, const int);
+void IspisLjestviceIme(PLAYER*, const int);
+void IspisLjestviceBodovi(PLAYER*, const int);
+void NatragNaIzbornik(void);
+
+#endif 
