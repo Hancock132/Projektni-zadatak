a45684a1d2abce32f272be4c007ce174f51c8a9a
Hancock132 <105845679+Hancock132@users.noreply.github.com>
2022-09-27 22:21:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
-int main(void)
-{
-	Izbornik();
+int main() {
+	int uvjet = 1;
+	printf("***SKLADISTE ALKOHOLNIH PICA***\n\n");
+
+	while (uvjet != 99)
+		uvjet = izbornik();
+
 	return 0;
 }
