c96de30183d1be444086f49480f5d6fa1be98abd
Hancock132 <105845679+Hancock132@users.noreply.github.com>
2022-09-29 17:40:44 +0200

    Create dataType.h

---
+++ dataType.h
@@ -0,0 +1,13 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+
+typedef struct student {
+	int id;
+	int godine;
+	char ime[25];
+	char prezime[25];
+	char adresa[50];
+	char smjer[25];
+}STUDENT;
+
+#endif
